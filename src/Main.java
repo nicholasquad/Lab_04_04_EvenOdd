734bde735593b0d045ff8633e7758ef89de797dd
nicholasquad <nicholasmash2006@gmail.com>
2024-09-23 16:23:58 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        int evenNumber = 2;
+        int oddNumber = 3;
+        int Even_Number;
+        int Odd_Number;
+
+        evenNumber = Even_Number = 2;
+        oddNumber = Odd_Number = 3;
+
+        System.out.println("The odd number is " + oddNumber);
+        System.out.println("The even number is " + evenNumber);
+
+    }
+}
